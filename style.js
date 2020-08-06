758f2576b1c7c9927615fafcd3c0a92a1283cc1d
raydphillips3 <64376825+raydphillips3@users.noreply.github.com>
2020-08-06 00:31:12 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,24 @@
+document.getElementById("gitHub").addEventListener("click", function() {
+    window.open("https://github.com/raydphillips3");
+});
+
+
+document.getElementById("resume").addEventListener("click", function() {
+   window.open("RaymondPhillipsResume.pdf");
+});
+
+document.getElementById("proj").addEventListener("click", function() {
+   window.open("https://pet-n-parent.herokuapp.com/");
+});
+
+document.getElementById("sched").addEventListener("click", function() {
+   window.open("https://raydphillips3.github.io/HM-5/");
+});
+
+document.getElementById("hobby").addEventListener("click", function() {
+   window.open("https://www.facebook.com/djray1906/");
+});
+
+document.getElementById("cinesearch").addEventListener("click", function() {
+   window.open("https://alextagg392.github.io/CineSearch/");
+});

481b604c860dc34b61037a4a779447e09dbf3816
raydphillips3 <64376825+raydphillips3@users.noreply.github.com>
2020-08-05 22:48:02 -0500

    Delete style.js

--- style.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("gitHub").addEventListener("click", function() {
-    window.open("https://github.com/raydphillips3");
-});
-
-
-document.getElementById("resume").addEventListener("click", function() {
-   window.location.href="RaymondPhillipsResume.pdf";
-});
